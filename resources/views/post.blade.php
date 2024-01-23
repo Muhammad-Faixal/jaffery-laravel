ab89da031e952525db54ee9b4271b601d596b8d4
Muhammad-Faixal <faisalshareef229@gmail.com>
2024-01-24 03:11:58 +0500

    some code added

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,2 +1,21 @@
-heya
-<a href="/">Go Back</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+
+        <?= $post; ?>
+    </article>
+    <a href="/">Go Back</a>
+
+</body>
+
+</html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -13,28 +13,28 @@
 
 <body>
     <article>
-        <h1><a href="/post">First Post</a></h1>
+        <h1><a href="/posts/first-post">First Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam quod alias velit minus animi amet
             commodi eius officia mollitia consequatur ratione nisi hic sequi voluptates libero aliquam, quibusdam
             laudantium impedit perferendis similique recusandae, accusamus suscipit dicta. Inventore, beatae eius?</p>
     </article>
 
     <article>
-        <h1><a href="/post">Second Post</a></h1>
+        <h1><a href="/posts/second-post">Second Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam quod alias velit minus animi amet
             commodi eius officia mollitia consequatur ratione nisi hic sequi voluptates libero aliquam, quibusdam
             laudantium impedit perferendis similique recusandae, accusamus suscipit dicta. Inventore, beatae eius?</p>
     </article>
 
     <article>
-        <h1><a href="/post">Third Post</a></h1>
+        <h1><a href="/posts/third-post">Third Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam quod alias velit minus animi amet
             commodi eius officia mollitia consequatur ratione nisi hic sequi voluptates libero aliquam, quibusdam
             laudantium impedit perferendis similique recusandae, accusamus suscipit dicta. Inventore, beatae eius?</p>
     </article>
 
     <article>
-        <h1><a href="/post">Fourth Post</a></h1>
+        <h1><a href="/posts/fourth-post">Fourth Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam quod alias velit minus animi amet
             commodi eius officia mollitia consequatur ratione nisi hic sequi voluptates libero aliquam, quibusdam
             laudantium impedit perferendis similique recusandae, accusamus suscipit dicta. Inventore, beatae eius?</p>
